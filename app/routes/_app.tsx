d27823ce0a373b43ee2da84d86e29ad793673b45
Maksim Peskov <kostarsf@gmail.com>
2023-05-20 23:03:17 +0300

    hide logout button when not logged in

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -5,19 +5,30 @@ export default function AppLayout() {
   const user = useOptionalUser();
 
   return (
-    <div className='px-6 md:px-16 lg:px-32 pt-3 md:pt-6 pb-24 m-auto'>
-      <div className='flex justify-between items-baseline mb-6 md:mb-12'>
-        <Link to='/' className='font-serif text-xl font-bold tracking-wider transition-colors hover:text-blue-500'>
+    <div className='m-auto px-6 pb-24 pt-3 md:px-16 md:pt-6 lg:px-32'>
+      <div className='mb-6 flex items-baseline justify-between md:mb-12'>
+        <Link
+          to='/'
+          className='font-serif text-xl font-bold tracking-wider transition-colors hover:text-blue-500'
+        >
           Креатур
         </Link>
         {user ? (
-          <Link to='/user' className='block hover:underline'>
-            ID {user.id.split("-")[0].toUpperCase()}
+          <>
+            <Link to='/user' className='block hover:underline'>
+              ID {user.id.split("-")[0].toUpperCase()}
+            </Link>
+            <Form method='POST' action='/logout'>
+              <button type='submit' className='hover:underline'>
+                Выход
+              </button>
+            </Form>
+          </>
+        ) : (
+          <Link to='/login' className='hover:underline'>
+            Вход
           </Link>
-        ) : null}
-        <Form method='POST' action='/logout'>
-          <button type='submit' className="hover:underline">Выход</button>
-        </Form>
+        )}
       </div>
       <Outlet />
     </div>
